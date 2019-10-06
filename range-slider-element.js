143368053306f01db2a5813cd3a6a1a9b03bf5e1
André Ruffert <andre@andreruffert.com>
2019-10-06 02:28:55 +0200

    feat: add keyboard & a11y support  + refactoring

--- range-slider-element.js
+++ range-slider-element.js
@@ -3,12 +3,29 @@ import * as style from './styles.css';
 const UPDATE_EVENTS = ['input', 'change'];
 const REFLECTED_ATTRIBUTES = ['min', 'max', 'step', 'value', 'disabled', 'value-precision'];
 
+const ARIA_ATTRIBUTES = {
+  value: 'valuenow',
+  min: 'valuemin',
+  max: 'valuemax',
+};
+
+const TEMPLATE = `
+  <div class="thumb-wrapper">
+    <div class="thumb"></div>
+  </div>
+`;
+
 class RangeSliderElement extends HTMLElement {
   constructor() {
     super();
     this._ignoreChange = false;
-    // this._shadowRoot = this.attachShadow({ mode: 'open' });
-    // this._shadowRoot.innerHTML = '<div class="wrapper"></div>';
+
+    // Aria attributes
+    this.setAttribute('tabindex', '0');
+    this.setAttribute('role', 'slider');
+    setAriaAttribute(this, 'value', this.value);
+    setAriaAttribute(this, 'min', this.min);
+    setAriaAttribute(this, 'max', this.max);
   }
 
   static get observedAttributes() {
@@ -26,45 +43,41 @@ class RangeSliderElement extends HTMLElement {
   get step() { return this.getAttribute('step') || '1'; }
   get value() { return this.getAttribute('value') || this._defaultValue; }
   get disabled() { return this.getAttribute('disabled') || false }
-  get valuePrecision() { return this.getAttribute('label-precision') || ''; }
+  get valuePrecision() { return this.getAttribute('value-precision') || ''; }
 
   set min(min) { this.setAttribute('min', min); }
   set max(max) { this.setAttribute('max', max); }
   set step(step) { this.setAttribute('step', step); }
   set value(value) { this.setAttribute('value', value); }
   set disabled(disabled) { this.setAttribute('disabled', disabled); }
-  set valuePrecision(precision) { this.setAttribute('label-precision', precision); }
+  set valuePrecision(precision) { this.setAttribute('value-precision', precision); }
 
   connectedCallback() {
-    if (this.firstChild) return;
+    if (!this.firstChild) {
+      this.innerHTML = TEMPLATE;
+    }
 
     this.addEventListener('pointerdown', this._startHandler, false);
     this.addEventListener('pointerup', this._endHandler, false);
-
-    this.innerHTML = `
-      <div class="thumb-wrapper">
-        <div class="thumb"></div>
-        <div class="value-display"></div>
-      </div>
-    `;
-
-    this._valueDisplay = this.querySelector('.value-display');
+    this.addEventListener('keydown', this._keyCodeHandler, false);
     this._update();
   }
 
   disconnectedCallback() {
     this.removeEventListener('pointerdown', this._startHandler, false);
     this.removeEventListener('pointerup', this._endHandler, false);
+    this.removeEventListener('keydown', this._keyCodeHandler, false);
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (this._ignoreChange) return;
+    if (oldValue === newValue || this._ignoreChange) return;
     this._update();
+    setAriaAttribute(this, name, newValue);
   }
 
   _startHandler = e => {
     this.classList.add('touch-active');
-
+    this.focus();
     // Click and drag
     this.setPointerCapture(e.pointerId);
     this.addEventListener('pointermove', this._moveHandler, false);
@@ -76,17 +89,31 @@ class RangeSliderElement extends HTMLElement {
   }
 
   _moveHandler = e => {
-    // e.preventDefault();
     this._reflectValue(e);
   }
 
   _endHandler = e => {
+    // alert(e.type);
     this.classList.remove('touch-active');
     this.releasePointerCapture(e.pointerId);
     this.removeEventListener('pointermove', this._moveHandler, false);
 
     // TODO: check if value changed
     this.dispatchEvent(new Event('change', { bubbles: true }));
+  }
+
+  _keyCodeHandler = e => {
+    e.preventDefault();
+    const code = e.code;
+    const up = ['ArrowUp', 'ArrowRight'].includes(code);
+    const down = ['ArrowDown', 'ArrowLeft'].includes(code);
+
+    if (up) {
+      this._stepUp();
+    }
+    else if (down) {
+      this._stepDown();
+    }
   }
 
   _reflectValue = event => {
@@ -114,7 +141,7 @@ class RangeSliderElement extends HTMLElement {
     // Value precision
     const newValue = valuePrecision ? nearestValue.toFixed(valuePrecision) : Math.round(nearestValue).toString();
 
-    console.log({ fullWidth, offsetX, oldValue, computedValue, newValue});
+    // console.log({ fullWidth, offsetX, oldValue, computedValue, newValue});
 
     if (oldValue !== newValue) {
       this.value = newValue;
@@ -122,12 +149,10 @@ class RangeSliderElement extends HTMLElement {
     }
   }
 
-  _stepUp(amount = this.step) {
-    this.value += amount;
-  }
-
-  _stepDown(amount = this.step) {
-    this.value -= amount;
+  _constrainValue(value) {
+    const min = Number(this.min);
+    const max = Number(this.max);
+    return Math.min(Math.max(value, min), max);
   }
 
   _update() {
@@ -136,18 +161,42 @@ class RangeSliderElement extends HTMLElement {
     const value = Number(this.value);
     const percent = (100 * (value - min)) / (max - min);
 
-    if (this._valueDisplay) {
-      this._valueDisplay.textContent = value;
-    }
+    console.log('update', { value });
 
     this.style.setProperty('--value-percent', percent + '%');
     this.style.setProperty('--value-width', '' + this.value.length);
+  }
+
+  stepUp(amount = this.step) {
+    const oldValue = Number(this.value);
+    const newValue = this._constrainValue(oldValue + Number(amount));
+    if (oldValue !== newValue) {
+      this.value = newValue;
+      this.dispatchEvent(new Event('input', { bubbles: true }));
+      this.dispatchEvent(new Event('change', { bubbles: true }));
+    }
+  }
+
+  stepDown(amount = this.step) {
+    const oldValue = Number(this.value);
+    const newValue = this._constrainValue(oldValue - Number(amount));
+    if (oldValue !== newValue) {
+      this.value = newValue;
+      this.dispatchEvent(new Event('input', { bubbles: true }));
+      this.dispatchEvent(new Event('change', { bubbles: true }));
+    }
   }
 }
 
 function getPrescision(value = '') {
   const afterDecimal = value.split('.')[1];
   return afterDecimal ? afterDecimal.length : 0;
+}
+
+function setAriaAttribute(element, name, value) {
+  const attributeName = ARIA_ATTRIBUTES[name];
+  if (!attributeName) return;
+  element.setAttribute(`aria-${attributeName}`, value);
 }
 
 export default RangeSliderElement;
