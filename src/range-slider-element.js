8d5325bb86808c2b136657d38aec421058c0aed9
André Ruffert <github@andreruffert.com>
2025-02-17 01:12:29 +0100

    feat: introduce multi thumb support

--- src/range-slider-element.js
+++ src/range-slider-element.js
@@ -1,10 +1,6 @@
-const REFLECTED_ATTRIBUTES = ['min', 'max', 'step', 'value', 'disabled', 'value-precision'];
+import { getPrescision, setAriaAttribute } from './utils';
 
-const ARIA_ATTRIBUTES = {
-  value: 'valuenow',
-  min: 'valuemin',
-  max: 'valuemax',
-};
+const REFLECTED_ATTRIBUTES = ['min', 'max', 'step', 'value', 'disabled', 'value-precision'];
 
 const TEMPLATE = document.createElement('template');
 TEMPLATE.innerHTML = `
@@ -22,44 +18,46 @@ export default class RangeSliderElement extends HTMLElement {
   static formAssociated = true;
 
   #internals;
-  #value;
-
-  #isVertical = this.getAttribute('orientation') === 'vertical';
-  #isRTL = this.#isVertical || this.getAttribute('dir') === 'rtl';
+  #value = [];
+  #valuePercent = [];
+  #thumbIndex = 0;
 
   constructor() {
     super();
     // Get access to the internal form control APIs
     this.#internals = this.attachInternals();
 
-    // Set initial value
-    this.value = this.getAttribute('value') || this.#getComputedValue();
-
-    // Enable focus
-    !this.disabled && this.setAttribute('tabindex', '0');
-
-    // Set aria attributes
-    this.setAttribute('role', 'slider');
-    setAriaAttribute(this, 'value', this.value);
-    setAriaAttribute(this, 'min', this.min);
-    setAriaAttribute(this, 'max', this.max);
-
+    // Template setup
     if (!this.firstChild) {
-      this.append(TEMPLATE.content.cloneNode(true));
+      this.appendChild(TEMPLATE.content.cloneNode(true));
     }
+
+    // Thumb setup
+    this.#thumbs.forEach((thumb, index) => {
+      thumb.dataset.thumb = index;
+      thumb.setAttribute('role', 'slider');
+      setAriaAttribute(thumb, 'min', this.min);
+      setAriaAttribute(thumb, 'max', this.max);
+      if (!this.disabled) {
+        thumb.setAttribute('tabindex', 0);
+      }
+    });
+
+    // Set initial value
+    this.value = this.getAttribute('value') || this.#getDefaultValue();
   }
 
   get min() {
-    return this.getAttribute('min') || '0';
+    return this.hasAttribute('min') ? Number(this.getAttribute('min')) : 0;
   }
   get max() {
-    return this.getAttribute('max') || '100';
+    return this.hasAttribute('max') ? Number(this.getAttribute('max')) : 100;
   }
   get step() {
-    return this.getAttribute('step') || '1';
+    return this.hasAttribute('step') ? Number(this.getAttribute('step')) : 1;
   }
   get value() {
-    return this.#value;
+    return this.#value.join(',');
   }
   get disabled() {
     return this.getAttribute('disabled') === '' || false;
@@ -67,28 +65,58 @@ export default class RangeSliderElement extends HTMLElement {
   get valuePrecision() {
     return this.getAttribute('value-precision') || '';
   }
+  get #isVertical() {
+    return Boolean(this.getAttribute('orientation') === 'vertical');
+  }
+  get #isRTL() {
+    return Boolean(this.#isVertical || this.getAttribute('dir') === 'rtl');
+  }
+  get #isMultiThumb() {
+    return this.#thumbs.length > 1;
+  }
+  get #thumbs() {
+    return this.querySelectorAll('[data-thumb]');
+  }
+  get #trackFill() {
+    return this.querySelector('[data-track-fill]');
+  }
+  get #size() {
+    return this.#isVertical ? this.offsetHeight : this.offsetWidth;
+  }
 
   set min(min) {
     this.setAttribute('min', min);
+    for (const thumb of this.#thumbs) {
+      setAriaAttribute(thumb, 'min', min);
+    }
   }
   set max(max) {
     this.setAttribute('max', max);
+    for (const thumb of this.#thumbs) {
+      setAriaAttribute(thumb, 'max', max);
+    }
   }
   set step(step) {
     this.setAttribute('step', step);
   }
-  set value(value) {
-    this.#value = this.#getSaveValue(value);
-    this.#internals.setFormValue(this.#value);
-    this.#update();
+  set value(values) {
+    String(values)
+      .split(',')
+      .map((value, index) => {
+        this.#updateValue(index, value);
+      });
   }
   set disabled(disabled) {
     if (disabled) {
       this.setAttribute('disabled', '');
-      this.removeAttribute('tabindex');
+      for (const thumb of this.#thumbs) {
+        thumb.removeAttribute('tabindex');
+      }
     } else {
       this.removeAttribute('disabled');
-      this.setAttribute('tabindex', 0);
+      for (const thumb of this.#thumbs) {
+        thumb.setAttribute('tabindex', 0);
+      }
     }
   }
   set valuePrecision(precision) {
@@ -127,39 +155,54 @@ export default class RangeSliderElement extends HTMLElement {
   }
 
   connectedCallback() {
+    this.addEventListener('focusin', this.#focusHandler);
     this.addEventListener('pointerdown', this.#startHandler);
     this.addEventListener('keydown', this.#keyCodeHandler);
   }
 
   disconnectedCallback() {
+    this.removeEventListener('focusin', this.#focusHandler);
     this.removeEventListener('pointerdown', this.#startHandler);
     this.removeEventListener('keydown', this.#keyCodeHandler);
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (oldValue === newValue) return;
-    this.#update();
-    setAriaAttribute(this, name, newValue);
+
+    // Value update
+    if (name === 'value') {
+      this.value = newValue;
+    }
+    // General update
+    else {
+      this.value = this.value;
+    }
   }
+
+  #focusHandler = (event) => {
+    this.#thumbIndex = Number(event.target.dataset.thumb);
+  };
 
   #startHandler = (event) => {
     if (this.disabled) return;
 
-    event.stopPropagation();
-
-    // Click and drag
     this.setPointerCapture(event.pointerId);
     this.addEventListener('pointermove', this.#moveHandler);
     window.addEventListener('pointerup', this.#endHandler);
     window.addEventListener('pointercancel', this.#endHandler);
 
     // Click jump (ignore thumb clicks)
-    if (event.target?.dataset?.thumb !== undefined) return;
-    this.#reflectValue(event);
+    if (event.target?.dataset?.thumb === undefined) {
+      const { offsetX, offsetY } = event;
+      this.#thumbIndex = this.#getClosestThumb(this.#isVertical ? offsetY : offsetX);
+      this.#mirrorValue(this.#isVertical ? offsetY : offsetX);
+    }
   };
 
   #moveHandler = (event) => {
-    this.#reflectValue(event);
+    event.stopPropagation();
+    event.preventDefault();
+    this.#mirrorValue(this.#isVertical ? event.offsetY : event.offsetX);
   };
 
   #endHandler = (event) => {
@@ -185,97 +228,156 @@ export default class RangeSliderElement extends HTMLElement {
     }
   };
 
-  #reflectValue = (event) => {
-    const isVertical = Boolean(this.#isVertical);
-    const isRTL = Boolean(this.#isRTL);
-    const min = Number(this.min);
-    const max = Number(this.max);
-    const oldValue = this.value;
-    const fullSize = isVertical ? this.offsetHeight : this.offsetWidth;
-    const offset = Math.min(Math.max(isVertical ? event.offsetY : event.offsetX, 0), fullSize);
-    const percentage = offset / fullSize;
-    const percentageComplete = isRTL ? 1 - percentage : percentage;
-
-    // Fit the percentage complete between the range [min,max]
-    // by remapping from [0, 1] to [min, min+(max-min)].
-    const computedValue = min + percentageComplete * (max - min);
-
-    // Constrain value
-    const newValue = this.#constrainValue(computedValue);
-
-    if (oldValue !== newValue) {
-      this.value = newValue;
-      this.dispatchEvent(new Event('input', { bubbles: true }));
-      setAriaAttribute(this, 'value', this.value);
-    }
+  /**
+   *
+   * @param {number} offset
+   */
+  #mirrorValue = (offset) => {
+    const safeOffset = Math.min(Math.max(offset, 0), this.#size);
+    const percent = safeOffset / this.#size;
+    const computedValue = this.#getValueFromPercent(this.#isRTL ? 1 - percent : percent);
+    this.#updateValue(this.#thumbIndex, computedValue);
   };
 
-  #update() {
-    const min = Number(this.min);
-    const max = Number(this.max);
-    const value = Number(this.value);
-    const percentage = (100 * (value - min)) / (max - min);
-    this.style.setProperty('--value-percentage', `${percentage}%`);
+  #getDefaultValue() {
+    return this.max < this.min ? this.min : this.min + (this.max - this.min) / 2;
   }
 
-  #constrainValue(value) {
-    const step = Number(this.step);
-    const valuePrecision = Number(this.valuePrecision) || getPrescision(this.step) || 0;
+  /**
+   *
+   * @param {number} value
+   * @returns
+   */
+  #getPercentFromValue(value) {
+    return (100 * (value - this.min)) / (this.max - this.min);
+  }
 
-    // min, max constrain
-    const saveValue = this.#getSaveValue(value);
+  /**
+   * Fit the percentage complete between the range [min,max]
+   * by remapping from [0, 1] to [min, min+(max-min)].
+   *
+   * @param {number} percent
+   * @returns
+   */
+  #getValueFromPercent(percent) {
+    return this.min + percent * (this.max - this.min);
+  }
+
+  /**
+   *
+   * @param {number} offset
+   * @returns
+   */
+  #getClosestThumb(offset) {
+    let closestThumb;
+    const safeOffset = Math.min(Math.max(offset, 0), this.#size);
+    const percent = safeOffset / this.#size;
+    const value = this.#getValueFromPercent(this.#isRTL ? 1 - percent : percent);
+
+    // Split #value based on the first thumb position from the "end"
+    const split = this.#value.findIndex((v) => value - v < 0);
+    // Pick the first one
+    if (split === 0) {
+      closestThumb = split;
+    }
+    // Pick the last one (position is past all the thumbs)
+    else if (split === -1) {
+      closestThumb = this.#value.length - 1;
+    } else {
+      const lastStart = this.#value[split - 1];
+      const firstEnd = this.#value[split];
+      // Pick the first one from the "start" unless they are stacked on top of each other
+      if (Math.abs(lastStart - value) < Math.abs(firstEnd - value)) {
+        closestThumb = split - 1;
+      }
+      // Pick the last one from the "end"
+      else {
+        closestThumb = split;
+      }
+    }
+
+    return closestThumb;
+  }
+
+  /**
+   *
+   * @param {number} index
+   * @param {number} value
+   * @param {Array} dispatchEvents
+   */
+  #updateValue(index, value, dispatchEvents = ['input']) {
+    const oldValue = this.#value[index];
+    const valuePrecision = Number(this.valuePrecision) || getPrescision(this.step) || 0;
+    const thumbMinValue = this.#value[index - 1] || this.min;
+    const thumbMaxValue = this.#value[index + 1] || this.max;
+
+    // Thumb min, max constrain
+    const safeValue = Math.min(Math.max(value, thumbMinValue), thumbMaxValue);
 
     // Rounding in steps
-    const nearestValue = Math.round(saveValue / step) * step;
+    const nearestValue = Math.round(safeValue / this.step) * this.step;
 
     // Value precision
-    const newValue = valuePrecision
-      ? nearestValue.toFixed(valuePrecision)
-      : Math.round(nearestValue).toString();
+    const newValue = Number(
+      valuePrecision ? nearestValue.toFixed(valuePrecision) : Math.round(nearestValue),
+    );
 
-    return newValue;
+    if (oldValue !== newValue) {
+      console.log('#updateValue', { oldValue, newValue, index });
+
+      this.#value[index] = newValue;
+      this.#valuePercent[index] = this.#getPercentFromValue(newValue);
+      this.#internals.setFormValue(this.#value.join(','));
+      this.#updateThumb(index, newValue);
+      this.#updateTrackFill();
+      dispatchEvents.map((event) => {
+        this.dispatchEvent(new Event(event, { bubbles: true }));
+      });
+    }
   }
 
-  #getComputedValue() {
-    const min = Number(this.min);
-    const max = Number(this.max);
-    return String(max < min ? min : min + (max - min) / 2);
+  /**
+   *
+   * @param {number} index
+   * @param {number} value
+   */
+  #updateThumb(index, value) {
+    this.#thumbs[index].style.setProperty(
+      `inset-${this.#isVertical ? 'block' : 'inline'}-${this.#isVertical ? 'end' : 'start'}`,
+      `${this.#getPercentFromValue(value)}%`,
+    );
+    setAriaAttribute(this.#thumbs[index], 'value', value);
   }
 
-  #getSaveValue(value) {
-    const min = Number(this.min);
-    const max = Number(this.max);
-    return Math.min(Math.max(value, min), max);
+  #updateTrackFill() {
+    const trackFillStart = this.#isMultiThumb ? `${this.#valuePercent[0]}%` : 0;
+    const trackFillEnd = this.#isMultiThumb
+      ? `${100 - this.#valuePercent[this.#valuePercent.length - 1]}%`
+      : `${100 - this.#valuePercent[0]}%`;
+
+    this.#trackFill.style.setProperty(
+      `inset-${this.#isVertical ? 'block' : 'inline'}`,
+      this.#isVertical ? `${trackFillEnd} ${trackFillStart}` : `${trackFillStart} ${trackFillEnd}`,
+    );
   }
 
+  /**
+   *
+   * @param {number} amount - Amount to step up
+   */
   stepUp(amount = this.step) {
-    const oldValue = Number(this.value);
-    const newValue = this.#constrainValue(oldValue + Number(amount));
-    if (oldValue !== newValue) {
-      this.value = newValue;
-      this.dispatchEvent(new Event('input', { bubbles: true }));
-      this.dispatchEvent(new Event('change', { bubbles: true }));
-    }
+    console.log(this.#thumbIndex);
+
+    const newValue = this.#value[this.#thumbIndex] + amount;
+    this.#updateValue(this.#thumbIndex, newValue, ['change']);
   }
 
+  /**
+   *
+   * @param {number} amount - Amount to step down
+   */
   stepDown(amount = this.step) {
-    const oldValue = Number(this.value);
-    const newValue = this.#constrainValue(oldValue - Number(amount));
-    if (oldValue !== newValue) {
-      this.value = newValue;
-      this.dispatchEvent(new Event('input', { bubbles: true }));
-      this.dispatchEvent(new Event('change', { bubbles: true }));
-    }
+    const newValue = this.#value[this.#thumbIndex] - amount;
+    this.#updateValue(this.#thumbIndex, newValue, ['change']);
   }
-}
-
-function getPrescision(value = '') {
-  const afterDecimal = value.split('.')[1];
-  return afterDecimal ? afterDecimal.length : 0;
-}
-
-function setAriaAttribute(element, name, value) {
-  const attributeName = ARIA_ATTRIBUTES[name];
-  if (!attributeName) return;
-  element.setAttribute(`aria-${attributeName}`, value);
 }
